0470f8a927f747036c4a9158d7fe95852ab1c625
d-cdmi <cdmi.d20@gmail.com>
2024-03-21 22:01:27 +0530

    21-03-2024 22:01

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import os
+from pytz import timezone 
+from datetime import datetime
+
+ind_time = datetime.now(timezone("Asia/Kolkata")).strftime(f'%d-%m-%Y %H:%M')
+print(ind_time)
 
 os.system("git add .")
-os.system('git commit -m "21-03-2024 N" ')
+os.system(f'git commit -m "{ind_time}" ')
 os.system("git push")
+
+
+
+# timestamp = time.strftime('%H:%M:%S')
+# print(timestamp)
+# timestamp = time.strftime('%H')
+# print(timestamp)
+# timestamp = time.strftime('%M')
+# print(timestamp)
+# timestamp = time.strftime('%S')
+# print(timestamp)
+# https://docs.python.org/3/library/time.html#time.strftime
+
