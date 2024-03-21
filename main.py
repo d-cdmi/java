ac951b1bf47f74b76856cef31d9f8aae7367d80d
d-cdmi <cdmi.d20@gmail.com>
2024-03-21 22:01:53 +0530

    21-03-2024 | 22:01

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from pytz import timezone 
 from datetime import datetime
 
-ind_time = datetime.now(timezone("Asia/Kolkata")).strftime(f'%d-%m-%Y %H:%M')
+ind_time = datetime.now(timezone("Asia/Kolkata")).strftime(f'%d-%m-%Y | %H:%M')
 print(ind_time)
 
 os.system("git add .")
