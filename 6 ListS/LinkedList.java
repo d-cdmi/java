0b949e2c2d9fda38be8e99b5a2ddad4cc5243418
d-cdmi <cdmi.d20@gmail.com>
2024-03-22 23:01:02 +0530

    22-03-2024 | 23:01

--- 6 ListS/LinkedList.java
+++ 6 ListS/LinkedList.java
@@ -14,9 +14,11 @@ public class LinkedList {
     }
     public static Node head;
     public static Node tail;
+    public static int size;
 
     public void addfist(int data) {
         Node newNode = new Node(data);
+        size++;
         if(head == null) {
             head = tail = newNode;
             return;
@@ -26,6 +28,7 @@ public class LinkedList {
     }
     public void addlast(int data) {
         Node newNode = new Node(data);
+        size++;
         if(head == null) {
             head = tail = newNode;
             return;
@@ -52,6 +55,7 @@ public class LinkedList {
             return;
         }
         Node newNode = new Node(data);
+        size++;
         Node temp = head;
         int i = 0;
         
@@ -63,6 +67,74 @@ public class LinkedList {
         temp.next = newNode;
     }
 
+    public int removeFist() {
+        if(size==0) {
+            System.out.println("link List Is Mt");
+            return Integer.MIN_VALUE;
+        } else if (size==1) {
+            int val = head.data;
+            head = tail = null;
+            size=0;
+            return val;
+        }
+        int val = head.data;
+        head =head.next;
+        size--;
+        return val;
+    }
+    public int removeLast() {
+        if(size==0) {
+            System.out.println("link List Is Mt");
+            return Integer.MIN_VALUE;
+        } else if (size==1) {
+            int val = head.data;
+            head = tail = null;
+            size=0;
+            return val;
+        }
+        Node prev =  head;
+        for (int i =0;i< size-2;i++) {
+            prev = prev.next;
+        }
+        int val = tail.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+    public int itrSearch(int key) {
+        Node temp = head;
+        int i=0;
+        while (temp!=null) {
+            if (temp.data == key) {
+                return i;
+            }
+            i++;
+            temp = temp.next;
+        }
+        return -1;
+    }
+
+    public int helper(Node head, int key) {
+        if(head==null) {
+            return -1;
+        } 
+
+        if (head.data==key) {
+            return 0;
+        }
+        int idx = helper(head.next, key) ;
+        if (idx == -1) {
+            return -1;
+        }
+
+        return idx +1;
+    }
+
+    public int ReSerach(int key) {
+        return helper(head ,key);
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         ll.addfist(2);
@@ -70,8 +142,16 @@ public class LinkedList {
         ll.addlast(3);
         ll.addlast(4);
 
-        ll.add(2, 10);
-
-        ll.print();
+        // ll.print();
+        // ll.removeFist();
+        // ll.print();
+        // ll.removeLast();
+        // ll.print();
+        // System.out.println(ll.size);
+  
+        System.out.println(ll.ReSerach  (4 ));
+        System.out.println(ll.ReSerach(11));
+  
+  
     }
 }
